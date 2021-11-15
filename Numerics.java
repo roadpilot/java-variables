6de3d9bf003a5d820e1999788f9c460332a623c5
roadpilot <mdacht777@gmail.com>
2021-11-15 08:24:27 -0700

    inserted "_" for comma separator and comments

--- Numerics.java
+++ Numerics.java
@@ -1,7 +1,8 @@
 public class Numerics {
     public static void main(String[] args) {
-        int lowest = -2147483648;        
-        int highest = 2147483647;
+        int lowest = -2_147_483_648;        
+        int highest = 2_147_483_647;
+        // int data type will accept "_" as the comma "," separator for numbers but will accept ","
 
         double bigDecimalValue = 374374393485673.92348773838234;
         double scientificValue = 2.345123e-22;
@@ -10,6 +11,15 @@ public class Numerics {
         System.out.println("Highest: " + highest);        
 
         System.out.println("bigDecimalValue: " + bigDecimalValue);        
-        System.out.println("scientificValue: " + scientificValue);        
+        System.out.println("scientificValue: " + scientificValue); 
+
+        int quantity1 = 10;
+        //int quantity2 = 5.5;
+
+        double price1 = 29.99;
+        double price2 = 10; // double always has at least one trailing decimal "0" => 10.0
+
+        double result = quantity1 * price1;
+        System.out.println(result);
     }
 }
