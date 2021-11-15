904fd195289abaf77c50487b51aefc1d7575dda8
roadpilot <mdacht777@gmail.com>
2021-11-15 07:49:27 -0700

    initializing testing

--- Default.java
+++ Default.java
@@ -1,6 +1,21 @@
 public class Default {
     // main method
     public static void main(String[] args){
-        System.out.println("test");
+        String address = "123 Foo";
+        String customer;
+        String city;
+        String zip = "12345", country = "USA";
+
+        customer = "Steve";
+        city = "Rockwell";
+        String region = city;
+        String uncommon = new String("Don't do this");
+
+        System.out.println(address);
+        System.out.println(customer);
+        System.out.println(city);
+        System.out.println(zip);
+        System.out.println(country);
+
     }
 }
