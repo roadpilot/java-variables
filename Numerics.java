68f6ffaf22d58687d17d6b1ed8c1ceaa29984386
roadpilot <mdacht777@gmail.com>
2021-11-15 08:19:56 -0700

    variables initialized

--- Numerics.java
+++ Numerics.java
@@ -1,5 +1,15 @@
 public class Numerics {
     public static void main(String[] args) {
-        
+        int lowest = -2147483648;        
+        int highest = 2147483647;
+
+        double bigDecimalValue = 374374393485673.92348773838234;
+        double scientificValue = 2.345123e-22;
+
+        System.out.println("Lowest: " + lowest);        
+        System.out.println("Highest: " + highest);        
+
+        System.out.println("bigDecimalValue: " + bigDecimalValue);        
+        System.out.println("scientificValue: " + scientificValue);        
     }
 }
