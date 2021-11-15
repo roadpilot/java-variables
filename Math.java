0cb59a5a6b7d2e820b42d018e22ba3b7a1746a13
roadpilot <mdacht777@gmail.com>
2021-11-15 08:56:58 -0700

    test of math vs concat

---
+++ Math.java
@@ -0,0 +1,7 @@
+public class Math {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 5, num3 = 3;
+        System.out.println(num1 + num2 + num3);        
+    }
+    
+}
